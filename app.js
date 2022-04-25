96f37136cbd3876f38ccfc4dfb535b4b2cfc9985
boomafromboomiland <lubomira.kovacova@avon.com>
2022-04-25 11:53:33 +0200

    vytvorenie ľavej navigácie - img, h3, div

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 /*
-Co je za úkol v tomto projektu:
+//Co je za úkol v tomto projektu:
 
-1) Do prvku s id="recepty" vygeneruj z dat seznam všech receptů z naší "databáze".
-HTML vzor, jak vygenerovaný recept vypadá, je zakomentovaný v index.html.
+/*1) Do prvku s id="recepty" vygeneruj z dat seznam všech receptů z naší "databáze".
+HTML vzor, jak vygenerovaný recept vypadá, je zakomentovaný v index.html.*/
 
-2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat
+let recepty = document.querySelector('.recepty');
+let i = 0;
+receptyItems.forEach(nacitajZoznam);
+
+function nacitajZoznam() {
+  let recept = document.createElement('div');
+  recept.className = 'recept';
+  recepty.appendChild(recept);
+
+  let receptObrazek = document.createElement('div');
+  receptObrazek.className = 'recept-obrazek';
+  recept.appendChild(receptObrazek);
+
+  let obrazokReceptu = document.createElement('img');
+  obrazokReceptu.src = receptyItems[i].img;
+  receptObrazek.appendChild(obrazokReceptu);
+
+  let receptInfo = document.createElement('div');
+  receptInfo.className = 'recept-info';
+  recept.appendChild(receptInfo);
+
+  let nadpis = document.createElement('h3');
+  nadpis.innerHTML = receptyItems[i].nadpis;
+  receptInfo.appendChild(nadpis);
+
+  i++;
+
+}
+
+/*2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat
 by se měl seznam receptů vyfiltrovat podle hledaného slova.
 
 3) Doplň filtrovanání receptů podle kategorie.
