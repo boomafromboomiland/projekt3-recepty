ae22fa90e7607b33735e347dfddd633f5f515cd8
boomafromboomiland <lubomira.kovacova@avon.com>
2022-04-25 12:09:25 +0200

    zaindexovanie prvkov receptov

--- app.js
+++ app.js
@@ -11,38 +11,49 @@ receptyItems.forEach(nacitajZoznam);
 function nacitajZoznam() {
   let recept = document.createElement('div');
   recept.className = 'recept';
+  recept.setAttribute('dataI', i);
   recepty.appendChild(recept);
+
+  recept.addEventListener('click', ukazRecept);
 
   let receptObrazek = document.createElement('div');
   receptObrazek.className = 'recept-obrazek';
+  receptObrazek.setAttribute('dataI', i);
   recept.appendChild(receptObrazek);
 
   let obrazokReceptu = document.createElement('img');
   obrazokReceptu.src = receptyItems[i].img;
+  obrazokReceptu.setAttribute('dataI', i);
   receptObrazek.appendChild(obrazokReceptu);
 
   let receptInfo = document.createElement('div');
   receptInfo.className = 'recept-info';
+  receptInfo.setAttribute('dataI', i);
   recept.appendChild(receptInfo);
 
   let nadpis = document.createElement('h3');
   nadpis.innerHTML = receptyItems[i].nadpis;
+  nadpis.setAttribute('dataI', i);
   receptInfo.appendChild(nadpis);
 
   i++;
 
-}
+};
 
 /*2) Doplň hledání - v hlavičce odkomentuj pole pro hledání. Pri kliknutí na tlačítko Hledat
-by se měl seznam receptů vyfiltrovat podle hledaného slova.
+by se měl seznam receptů vyfiltrovat podle hledaného slova.*/
 
-3) Doplň filtrovanání receptů podle kategorie.
+// 3) Doplň filtrovanání receptů podle kategorie.
 
-4) Doplň řazení receptů podle hodnocení.
+// 4) Doplň řazení receptů podle hodnocení.
 
-5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
+/*5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
 Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie,
-recept-hodnoceni, recept-nazev, recept-popis.
+recept-hodnoceni, recept-nazev, recept-popis. */
 
-6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
-*/
+function ukazRecept(vybranyRecept) {
+  let index = vybranyRecept.target.getAttribute('dataI');
+  console.log('Klik na recept');
+};
+
+// 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
